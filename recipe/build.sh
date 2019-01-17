439e62672c20e247ad5022e96124ada999b6445b
Mark Piper <mark.piper@colorado.edu>
2019-01-17 13:39:10 -0700

    Set MACOSX_DEPLOYMENT_TARGET and disabled tests on macOS
    Setting MACOSX_DEPLOYMENT_TARGET to an empty string allows the project
    to be built on any macOS version.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
 
+export MACOSX_DEPLOYMENT_TARGET=""
+
 mkdir _build && cd _build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release
 make
-ctest
+if [[ `uname -s` == 'Linux' ]]; then
+    ctest
+fi
 make install
